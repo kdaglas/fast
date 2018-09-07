71ebd7413d942b08a41d98491b7d72153bfbf218
Douglas <daglach7@gmail.com>
2018-09-06 19:41:57 -0700

    Add the necessary files [Finishes #160278701]

---
+++ UI/js/fastfood.js
@@ -0,0 +1,27 @@
+function showRice() { 
+        document.getElementById('rice').style.display = "block";
+}
+
+function showMatooke() { 
+    document.getElementById('matooke').style.display = "block";
+}
+
+function showPosho() { 
+    document.getElementById('posho').style.display = "block";
+}
+
+function showPotatoes() { 
+    document.getElementById('potatoes').style.display = "block";
+}
+
+function showSpaghetti() { 
+    document.getElementById('spaghetti').style.display = "block";
+}
+
+function showFries() { 
+    document.getElementById('fries').style.display = "block";
+}
+
+function showJunk() { 
+    document.getElementById('junk').style.display = "block";
+}
