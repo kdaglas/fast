23d566fe54e62b622143b6c0833ca00e0d6c314e
Douglas <daglach7@gmail.com>
2018-09-07 17:19:12 -0700

    Add more responsiveness [#160343874]

--- UI/js/fastfood.js
+++ UI/js/fastfood.js
@@ -25,12 +25,3 @@ function showFries() {
 function showJunk() { 
     document.getElementById('junk').style.display = "block";
 }
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
