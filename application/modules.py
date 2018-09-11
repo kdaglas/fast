e01168e2fcba94ff8b3479e9165b43a52a30bf2d
Douglas <daglach7@gmail.com>
2018-09-11 16:26:44 -0700

    Add the files for the apis

---
+++ application/modules.py
@@ -0,0 +1,7 @@
+class Customer():
+
+    def __init__(self,username,emailaddress, password):
+        self.id = id
+        self.username = username
+        self.emailaddress = emailaddress
+        self.password = password
