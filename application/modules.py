8553f774bf9350d48d2c32f2ec7b6289f53d8ff2
Douglas <daglach7@gmail.com>
2018-09-13 11:13:35 -0700

    Add the necessary python files to run the api

--- application/modules.py
+++ application/modules.py
@@ -1,16 +1,23 @@
 class Customer():
 
-    def __init__(self,username,emailaddress, password):
-        self.id = id
+    def __init__(self, user_id, username, contact, password):
+        self.user_id = user_id
         self.username = username
-        self.emailaddress = emailaddress
+        self.contact = contact
         self.password = password
 
 
-class Order():
+class Meal():
 
-    def __init__(self, id, meal, price, today):
-        self.id = id
+    def __init__(self, meal_id, meal, price):
+        self.meal_id = meal_id
         self.meal = meal
         self.price = price
+
+
+class Order(Meal):
+
+    def __init__(self, order_id, meal_id, meal, price, today):
+        Meal.__init__(self, meal_id, meal, price)
+        self.order_id = order_id
         self.today = today
