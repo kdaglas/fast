e18adc8cceb01a2f3551ca5603fd6188f8ea3e3b
Douglas <daglach7@gmail.com>
2018-09-15 15:24:52 +0300

    Add the quantity attribute

--- modules/models.py
+++ modules/models.py
@@ -1,14 +1,16 @@
 class Meal():
 
-    def __init__(self, mealId, food, price):
+    def __init__(self, mealId, thetype, food, price):
         self.mealId = mealId
+        self.thetype = thetype
         self.food = food
         self.price = price
 
 
 class Order(Meal):
 
-    def __init__(self, orderId, mealId, food, price, today):
-        Meal.__init__(self, mealId, food, price)
+    def __init__(self, orderId, mealId, thetype, food, price, quantity, today):
+        Meal.__init__(self, mealId, thetype, food, price)
         self.orderId = orderId
+        self.quantity = quantity
         self.today = today
