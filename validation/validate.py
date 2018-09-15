c41026588226fe5335b4151d7b51f04b7fda4315
Douglas <daglach7@gmail.com>
2018-09-15 17:50:29 +0300

    Create the class that allows the validate function to operate

--- validation/validate.py
+++ validation/validate.py
@@ -4,43 +4,69 @@ from flask import jsonify
 
 class Validate():
 
-    def __init__(self, thetype, food, price):
+    def __init__(self, mealId, thetype, food, price, quantity):
 
         # valiation class for the meal inputs
 
+        self.mealId = mealId
         self.thetype = thetype
         self.food = food
         self.price = price
+        self.quantity = quantity    
 
-    def validate_empty(self, food):
+    @classmethod
+    def validate_empty(self, thetype, food, price,):
 
         # method to validate my input
 
-        if food == '':
+        if thetype == '':
+            return jsonify({"message": "The type of food is missing"}), 400
+        elif not re.search("^[a-zA-Z]", thetype):
+            return jsonify({"message": "The type of food should be characters"}), 400
+        elif food == '':
             return jsonify({"message": "The food type is missing"}), 400
-        elif not re.search("^[a-zA-Z]", self.food):
+        elif not re.search("^[a-zA-Z]", food):
             return jsonify({"message": "The food type should be characters"}), 400
-
-        # if not self.price:
-        #     return jsonify({"message": "The price is missing"}), 400
-        # elif not re.search("^[0-9]", self.price):
-        #     return jsonify({"message": "The price should be numbers"}), 400
+        elif price == '':
+            return jsonify({"message": "The price is missing"}), 400
+        elif not re.search("^[0-9]", price):
+            return jsonify({"message": "The price should be numbers"}), 400
+        else:
+            return True
 
     def validate_id(self, mealId):
 
         # valiation class for the meal id
 
-        try:
-           mealId = int(mealId)
-        except ValueError:
-            return jsonify({"message": "Id should be an interger"}), 400
+        # try:
+        #    mealId = int(mealId)
+        # except ValueError:
+        #     return jsonify({"message": "Id should be an interger"}), 400
+
+        valid = ""
+        if type(self.mealId) is not int:
+            valid="The mealId should be an integer"     
+        elif not re.search("^[0-9]" , self.mealId):
+            valid="The mealId should be an integer"    
+        else:
+            valid = True
+        return valid
 
     def validate_quantity(self, quantity):
 
         # valiation class for the quantity
 
-        try:
-           quantity = int(quantity)
-        except ValueError:
-            return jsonify({"message": "The quantity should be an interger"}), 400
+        # try:
+        #    quantity = int(quantity)
+        # except ValueError:
+        #     return jsonify({"message": "The quantity should be an interger"}), 400
+
+        valid = ""
+        if type(self.quantity) is not int:
+            valid="The quantity should be an integer"     
+        elif not re.search("^[0-9]" , self.quantity):
+            valid="The quantity should be an integer"    
+        else:
+            valid = True
+        return valid
         
