4dc8b45f9df7b51ff9c9f7dea9777c0cd8343832
Douglas <daglach7@gmail.com>
2018-09-15 22:19:58 +0300

    Add a test for validation

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,12 @@
 import unittest
+from application.routes import webapp
+from modules.models import Meal, Order
 
 
 class TestApp(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.client = webapp.test_client()
+    
+    def tearDown(self):
+        self.client = None
