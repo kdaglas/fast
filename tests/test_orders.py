6cf419481bc508f849e7ee5eae0210ad854e4aef
Douglas <daglach7@gmail.com>
2018-09-16 11:45:40 +0300

    Change the arrangement of the test class methods

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -14,18 +14,6 @@ class Test_Orders(unittest.TestCase):
         self.client = None
 
 
-    def test_place_order(self):
-
-        # a test for successfully placing an order 
-
-        response = self.client.post("/api/v1/orders", data = json.dumps(
-            dict(mealId = "9870654386", quantity = "2")), content_type = 'application/json')
-                                
-        reply = json.loads(response.data)
-        self.assertEquals(reply["message"], "Order successfully made")
-        self.assertEquals(response.status_code, 201)
-
-
     def test_add_meal(self):
 
         # a test for successfully adding a meal 
@@ -36,4 +24,17 @@ class Test_Orders(unittest.TestCase):
                                 
         reply = json.loads(response.data)
         self.assertEquals(reply["message"], "Meal successfully added")
+        self.assertEquals(response.status_code, 201)
+
+
+    def test_place_order(self):
+
+        # a test for successfully placing an order 
+
+        response = self.client.post("/api/v1/orders", data = json.dumps(
+            dict(thetype = "Breakfast", food = "Milk and bread", 
+                 price = "2000", quantity = "2")), content_type = 'application/json')
+                                
+        reply = json.loads(response.data)
+        self.assertEquals(reply["message"], "Order successfully made")
         self.assertEquals(response.status_code, 201)
