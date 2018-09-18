0a91581432d8311c9ed4aa74432cdd01390a775b
Douglas <daglach7@gmail.com>
2018-09-18 11:00:23 -0700

    Add validation to the test for orders

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -1,7 +1,7 @@
 import unittest
 from application.routes import webapp
 from run import webapp
-from application.models import Customer, Order
+from modules.order_model import Order
 import json
 
 
@@ -15,39 +15,15 @@ class Test_Orders(unittest.TestCase):
         self.client = None
 
 
-    def test_registration(self):
-
-        #  a test for successful user registration 
-
-        response = self.client.post("/api/v1/register", data=json.dumps(
-            dict(username="Dag", contact="256755598090", 
-                 password="Dag123")), content_type='application/json') 
-
-        reply = json.loads(response.data)
-        self.assertEqual(reply["message"], "Customer successfully registered")
-        self.assertEqual(response.status_code, 201)
-
-
-    def test_user_login_successful(self):
-
-        # Test for successful login 
-
-        response = self.client.post("/api/v1/login", data = json.dumps(
-            dict(username = "Dag", password = "Dag123")), content_type = 'application/json')
-
-        reply = json.loads(response.data)
-        self.assertEqual(reply["message"], "Successfully logged in")
-        self.assertEqual(response.status_code, 201)
-
-
     def test_place_order(self):
 
         # a test for successfully placing an order 
 
         response = self.client.post("/api/v1/orders", data = json.dumps(
-            dict(orderId = "4536784291", thetype = "Breakfast", food = "Milk and bread", 
+            dict(customerId = "5674684890", orderId = "4536784291", thetype = "Breakfast", food = "Milk and bread", 
                  price = "2000", quantity = "2", today = "2018-09-16")), content_type = 'application/json')
                                 
         reply = json.loads(response.data)
-        self.assertEqual(reply["message"], "Order successfully placed")
-        self.assertEqual(response.status_code, 201)
+        # if valida
+        self.assertEqual(reply["message"], "Your order has been successfully placed")
+        self.assertEqual(response.status_code, 200)
