e9724186c5e58ba64c7f6f5f7b1c8e1b051c870f
Douglas <daglach7@gmail.com>
2018-09-26 21:32:43 -0700

    chore(modules): modify the documentation of the class models for the APIs
    Modify the doc strings to explain better the flow and implementation of the modules
    according to what the functionality they are to operate.
    
    [Finishes #160790663]

--- app/modules/order_model.py
+++ app/modules/order_model.py
@@ -1,12 +1,10 @@
-'''  Object classes for the order model '''
-
+'''Object classes for the order model'''
 all_orders = []
 
 class Order():
-
-    ''' Initialising the order class '''
+    
     def __init__(self, customerId, orderId, thetype, food, price, quantity, status, today):
-
+        '''Initialising the order class'''
         self.customerId = customerId
         self.orderId = orderId
         self.thetype = thetype
@@ -16,9 +14,9 @@ class Order():
         self.status = status
         self.today = today
     
-    ''' this method returns a dictionary format of the order class '''
+    
     def place_order(self):
-
+        '''this method returns a dictionary format of the order class'''
         order = {
             'customerId' : self.customerId,
             'orderId' : self.orderId,
@@ -29,36 +27,34 @@ class Order():
             'status' : 'Not covered',
             'today' : self.today
         }
-
         all_orders.append(order)
         return order
 
-    ''' this method returns the placed orders '''
+    
     @classmethod
     def get_all_orders(cls):
-
+        '''this method returns the placed orders'''
         if len(all_orders) > 0:
             return [order for order in all_orders]
         return {'There is an error': 'No orders found'}
 
-    ''' This method returns the one order '''
+    
     @classmethod
     def get_one_order(cls, orderId):
-
+        '''This method returns the one order'''
         if int(orderId) > 0:
             if len(all_orders) > 0:
                 for order in all_orders:
                     if order.get('orderId') == int(orderId):
                         return order
-
                 return {"message": "order doesnot exist"}
             return {"message": "No order has been registered yet"}
         return {"message": "Order id has to bigger than zero"}
         
-    ''' this method return the edited order '''
+    
     @classmethod
     def update_order(cls, orderId, status):
-
+        '''this method return the edited order'''
         for order in all_orders:
             if order.get('orderId') == int(orderId):
                 order['status'] = status
